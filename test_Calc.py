36d547968a3066fb41973a255ca9233f09fe5651
KhobeibM <94747127+KhobeibM@users.noreply.github.com>
2023-07-06 20:21:06 +0400

    Adding requirements and test_Calc

---
+++ test_Calc.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for the calculator library
+"""
+
+import Calc
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == Calc.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == Calc.subtract(4, 2)
