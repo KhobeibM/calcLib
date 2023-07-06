4fbd53860a1a89492fa30ca0a45a6d7fd71fa264
KhobeibM <94747127+KhobeibM@users.noreply.github.com>
2023-07-06 19:08:07 +0400

    Add and Subtract functions

---
+++ Calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+
+def add(firstNum, secondNum):
+    return firstNum + secondNum
+
+
+def subtract(firstNum, secondNum):
+    return firstNum - secondNum
