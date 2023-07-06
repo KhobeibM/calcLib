cd1f91b6eb4b08c27ffb43f6bb5186adb5dbd679
KhobeibM <94747127+KhobeibM@users.noreply.github.com>
2023-07-06 23:04:20 +0400

    Added a multiplication test

--- test_Calc.py
+++ test_Calc.py
@@ -14,4 +14,4 @@ class TestCalculator:
         assert 2 == Calc.subtract(4, 2)
 
     def test_multiplication(self):
-        assert 100 == Calc.multiply(10, 10)
+        assert 100 == Calc.multiply(11, 11)
